4d3fbaa532fc1f18f1df4e0db5df0332517a2b1d
emmflo <emmanuelflo@hotmail.fr>
2015-05-05 03:27:20 +0200

    add background, some tile changes.

--- test.py
+++ test.py
@@ -6,13 +6,15 @@ from pyglet.image.codecs.pil import PILImageDecoder
 tiles_image = pyglet.image.load('tiles.png', decoder=PILImageDecoder())
 tiles = pyglet.image.ImageGrid(tiles_image, 16, 16)
 
+background_image = pyglet.image.load('background.png', decoder=PILImageDecoder())
+
 level_image = pyglet.image.load('level.bmp', decoder=PILImageDecoder())
 level_rawimage = level_image.get_image_data()
 format = 'RGB'
 pitch = level_rawimage.width * len(format)
 level_rawimage_array = bytearray(level_rawimage.get_data(format, pitch))
 
-lookup_table_tiles = {(0,255,0):0, (0,255,255):4, (127,127,127):1, (153,153,153):2, (0,0,0):3}
+lookup_table_tiles = {(0,255,0):0, (0,255,255):4, (127,127,127):2, (153,153,153):1, (0,0,0):3, (0,127,0):16, (255,0,0):17}
 
 level_width = level_rawimage.width
 level_height = level_rawimage.height
@@ -21,7 +23,8 @@ tile_height = 32
 
 batch = pyglet.graphics.Batch()
 background = pyglet.graphics.OrderedGroup(0)
-foreground = pyglet.graphics.OrderedGroup(1)
+middleground = pyglet.graphics.OrderedGroup(1)
+foreground = pyglet.graphics.OrderedGroup(2)
 
 sprites = []
 level = []
@@ -31,7 +34,7 @@ for i in range(0, level_width*level_height*3, 3):
     g = level_rawimage_array[i+1]
     b = level_rawimage_array[i+2]
     level.append(tiles[lookup_table_tiles[(r, g, b)]])
-    sprites.append(pyglet.sprite.Sprite(level[-1], batch=batch, group=background))
+    sprites.append(pyglet.sprite.Sprite(level[-1], batch=batch, group=middleground))
 
 #sprites = [pyglet.sprite.Sprite(tiles[0], batch=batch, group=background) for _ in range(4*4)]
 
@@ -39,6 +42,9 @@ for index, sprite in enumerate(sprites):
     sprite.x = (index % level_width) * 32
     sprite.y = (index // level_height) * 32
     #print("x = {}; y = {}".format(sprite.x, sprite.y))
+
+sprites.append(pyglet.sprite.Sprite(background_image, batch=batch, group=background))
+sprites[-1].scale = 2
 
 window = pyglet.window.Window(level_width*tile_width, level_height*tile_width)
 
